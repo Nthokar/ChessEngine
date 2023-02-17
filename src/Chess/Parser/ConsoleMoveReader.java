283c0e51f1edba0ff7b89dda0656124531ab2524
nthokar <zhvl03@gmail.com>
2023-02-18 01:29:55 +0500

    add default moveReader which parse string e2e4 to 3 - 2 -> 3 - 4

---
+++ src/Chess/Parser/ConsoleMoveReader.java
@@ -0,0 +1,33 @@
+package Chess.Parser;
+
+import Chess.Desk.Cell;
+import Chess.Desk.Move;
+
+import java.util.Scanner;
+
+public class ConsoleMoveReader {
+    public static Move readMove(String str){
+        try {
+            return new Move(readCell(str.substring(0,2)), readCell(str.substring(2)));
+        }
+        catch (IllegalAccessException e)
+        {
+            throw new RuntimeException("wrong input");
+        }
+    }
+
+    public static Cell readCell(String str) throws IllegalAccessException {
+        if (str.length() != 2)
+            throw new IllegalAccessException();
+        return new Cell((int)'h' - (int)str.charAt(0), Character.getNumericValue(str.charAt(1)));
+    }
+
+    public static void main(String[] args) {
+        Scanner myObj = new Scanner(System.in);
+        for(;;){
+            var input = myObj.nextLine();
+            var move = readMove(input);
+            System.out.println(move);
+        }
+    }
+}
